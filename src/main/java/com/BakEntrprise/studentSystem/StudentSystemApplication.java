b5539dfbd2478381d3161161568e58da7580fd69
Bakary Sylla <syllabakary2002@gmail.com>
2023-01-20 07:27:22 -0800

    data base mySQL added

--- src/main/java/com/BakEntrprise/studentSystem/StudentSystemApplication.java
+++ src/main/java/com/BakEntrprise/studentSystem/StudentSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentSystemApplication {
-
+//change port 8080
 	public static void main(String[] args) {
 		SpringApplication.run(StudentSystemApplication.class, args);
 	}

c3cf7cfa25532513fe3e90cd6d6262903d86a2c8
Bakary Sylla <syllabakary2002@gmail.com>
2023-01-23 09:20:37 -0800

    StudentController update withe added method StudentServiceImpl update with @service

--- src/main/java/com/BakEntrprise/studentSystem/controller/StudentController.java
+++ src/main/java/com/BakEntrprise/studentSystem/controller/StudentController.java
@@ -1,4 +1,24 @@
 package com.BakEntrprise.studentSystem.controller;
 
+import com.BakEntrprise.studentSystem.model.Student;
+import com.BakEntrprise.studentSystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//we inject student here
+@RequestMapping("/student")
+
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+    //write the cod to save our database
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student is added";
+    }
 }
